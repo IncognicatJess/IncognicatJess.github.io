b263606a0b5debb95fa0a63b884ea16b8f7da48d
IncognicatJess <jaisyunurm@gmail.com>
2025-07-09 10:34:17 +0700

    Push All

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IncognicatJess.github.io
+
+Sesi Kuliah Dosen eksternal Pak Yusuf Fahmi Adi Putera
 Tugas PWA Sederhana
+
+Nama/NIM : Jaisyu Nur Muhammadi/0920240034
+Prodi/Kelas : TRPL/1A
---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = ["/", "/index.html", "/style.css", "/icon.png"];
+
+self.addEventListener("install", (event) => {
+  console.log("[ServiceWorker] Install");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
